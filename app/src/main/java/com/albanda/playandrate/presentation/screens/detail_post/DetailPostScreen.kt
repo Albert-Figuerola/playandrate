54ff549cb29a0d6cb42bf5ba9c8f493545f743ef
Albert <afiguerola@gaddexapp.com>
2025-11-09 08:52:51 +0100

    Creating post detail screen

--- app/src/main/java/com/albanda/playandrate/presentation/screens/detail_post/DetailPostScreen.kt
+++ app/src/main/java/com/albanda/playandrate/presentation/screens/detail_post/DetailPostScreen.kt
@@ -1,17 +1,31 @@
 package com.albanda.playandrate.presentation.screens.detail_post
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.albanda.playandrate.presentation.components.TopBar
 import com.albanda.playandrate.presentation.screens.detail_post.components.DetailPostContent
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun DetailPostScreen(navHostController: NavHostController, post: String) {
     Scaffold (
-        content = {
-            DetailPostContent(navHostController)
+        topBar = {
+            TopBar(
+                title = null,
+                upAvailable = true,
+                navHostController = navHostController,
+                onClick = { navHostController.popBackStack() }
+            )
+        },
+        content = { innerPadding ->
+            DetailPostContent(modifier = Modifier
+                .padding(innerPadding)
+                , navHostController
+            )
         }
     )
 }
